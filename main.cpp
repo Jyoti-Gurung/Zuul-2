cb38a5414f7a84f5a9797ec1762ea32c0690a70d
JyotiGurung <74558610+Jyoti-Gurung@users.noreply.github.com>
2021-12-14 19:12:39 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+Author: Jyoti gurung
+Date: December 14 2021
+Project: Zuul; text based adventure game with 15 "rooms"(5 items in them), picking em up and causing reactions etc
+*/
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+class room {
+  public:
+
+};
+
+vector <room*> storage;
+
+
+int main() {
+  cout << "Hello World!\n";
+} 
