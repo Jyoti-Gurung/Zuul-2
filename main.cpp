6aa7d986ad946332c6c91dfe3053d30e43bdc0e7
Jyoti Gurung <74558610+Jyoti-Gurung@users.noreply.github.com>
2022-01-06 07:06:21 +0000

    Cleaning up item drops

--- main.cpp
+++ main.cpp
@@ -15,31 +15,8 @@ void printInventory();
 void pickUp();
 
 vector <room*> storage;
-char rooms[15][12];
 
-char items[5][15] = {
-  {'G','o','m','u'},
-  {'S','a','b','r','e'},
-  {'K','e','y'},
-  {'G','o','l','d'},
-  {'G','o','l','d','e','n','k','e','y'}
-};
-
-//store picked up items
-char inventory[5][15] = {
-  {'x'},
-  {'x'},
-  {'x'},
-  {'x'},
-  {'x'}
-};
-
-//keep track of the current room
-int currentRoom = 12;
-
-int main() {
-
-  char rooms[15][12] = {
+ char rooms[15][12] = {
     {'M','a','i','n','d','e','c','k'},
     {'L','o','w','e','r','d','e','c','k'},
     {'F','i','g','u','r','e','h','e','a','d'},
@@ -57,6 +34,29 @@ int main() {
     {'P','o','o','p','d','e','c','k'}
   };
 
+char items[5][15] = {
+  {'$','$'},
+  {'K','e','y'},
+  {'G','o','m','u'},
+  {'S','a','b','r','e'},
+  {'G','o','l','d','k','e','y'}
+};
+
+//store picked up items
+char inventory[5][15] = {
+  {'x'},
+  {'x'},
+  {'x'},
+  {'x'},
+  {'x'}
+};
+
+//keep track of the current room
+int currentRoom = 12;
+
+//THE INFINITE LOOP OF TRAVEL, MAIN FUNCTION
+int main() {
+
   //adding the rooms to the storage vector
   for (int i = 0; i < 15; i++) {
     room* nameless = new room();  storage.push_back(nameless);
@@ -68,11 +68,17 @@ int main() {
     (storage[i])->setId(i);
   }
 
-  //assigning items to rooms: Lowerdeck, Crowsnest, R5, R1, R7
-  (storage[1])->setItem(items[0]);
-  (storage[3])->setItem(items[1]);
-  (storage[6])->setItem(items[2]);
-  (storage[4])->setItem(items[3]);
+  //setting rooms empty
+  char empty[1] = {'x'};
+  for (int i = 0; i < 15; i++) {
+    (storage[i])->setItem(empty);
+  }
+
+  //assigning items to rooms
+  (storage[4])->setItem(items[0]);
+  (storage[13])->setItem(items[1]);
+  (storage[1])->setItem(items[2]);
+  (storage[3])->setItem(items[3]);
   (storage[12])->setItem(items[4]);
 
   cout << endl << "Commands:" << endl << 
@@ -87,7 +93,7 @@ int main() {
   printRoom(currentRoom);
 
   int logic;
-  while (logic != 7 || logic!= 0) {
+  while (logic != 7) {
 
     cout << endl;
     cin >> logic;
@@ -137,21 +143,35 @@ void printInventory() {
 
 void pickUp() {
 
-  int i = currentRoom;
-  int currentItem = 9; 
+  int currentItem = strlen((storage[currentRoom])->getItem()); 
+  int itemOrder;
+
+  switch(currentItem) {
+    case 2:
+      itemOrder = 0;
+      break;
+    case 3:
+      itemOrder = 1;
+      break;
+    case 4:
+      itemOrder = 2;
+      break;
+    case 5:
+      itemOrder = 3;
+      break;
+    case 7:
+      itemOrder = 4;
+      break;
+  }
 
   for (int i = 0; i < 5; i++){
     if (strcmp(inventory[i], "x") == false) {
       for (int a = 0; a < currentItem; a++){
-        inventory[i][a]=items[4][a];
+        inventory[i][a]=items[itemOrder][a];
       }
-      
+      cout << endl << (storage[currentRoom])->getItem()  << " picked up!" << endl;
       break;
     }
   }
-
-  cout << endl << (storage[i])->getItem()  << " picked up!" << endl;
-
-  //(storage[i])->setItem(inventory[0]);
 
 }
