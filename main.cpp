3ad78849219f44c1a96e22fd1efd71307d5e6904
Jyoti Gurung <74558610+Jyoti-Gurung@users.noreply.github.com>
2022-01-06 08:02:04 +0000

    Clean up only.

--- main.cpp
+++ main.cpp
@@ -11,8 +11,10 @@ Project: Zuul; text based adventure game with 15 "rooms" & 5 items(in the whole 
 using namespace std;
 
 void printRoom(int i);
-void printInventory();
 void pickUp();
+void drop();
+void printInventory();
+void printMap(int i);
 
 vector <room*> storage;
 
@@ -52,7 +54,10 @@ char inventory[5][15] = {
 };
 
 //keep track of the current room
-int currentRoom = 12;
+int currentRoom = 14;
+
+//for directions aka map
+int one, two, three, four;
 
 //THE INFINITE LOOP OF TRAVEL, MAIN FUNCTION
 int main() {
@@ -87,33 +92,42 @@ int main() {
   endl << "3: Up" <<
   endl << "4: Down" <<
   endl << "5: Pick up Item" <<
-  endl << "6: Print Inventory" <<
-  endl << "7: Quit game" << endl;
+  endl << "6: Drop a Item" <<
+  endl << "7: Print Inventory" <<
+  endl << "8: Quit game" << endl;
 
   printRoom(currentRoom);
+  printMap(currentRoom);
 
   int logic;
-  while (logic != 7) {
+  while (logic != 8) {
 
     cout << endl;
     cin >> logic;
     switch(logic) {
       case 1:
-        
+        printRoom(one);
+        currentRoom = one;
         break;
       case 2:
-        
+        printRoom(two);
+        currentRoom = two;
         break;
       case 3:
-        
+        printRoom(three);
+        currentRoom = three;
         break;
       case 4:
-
+        printRoom(four);
+        currentRoom = four;
         break;
       case 5:
       pickUp();
         break;
       case 6:
+      drop();
+        break;
+      case 7:
       printInventory();
         break;
     }
@@ -121,8 +135,9 @@ int main() {
 
 } 
 
-//functions for commands
+//FUNCTIONS FOR COMMANDS
 
+//print room; name, item and room #
 void printRoom(int i) {
   cout 
   << endl 
@@ -132,19 +147,12 @@ void printRoom(int i) {
   << endl;  
 }
 
-void printInventory() {
-  
-  cout << endl << "Inventory:" << endl;
-
-  for (int i = 0; i < 5; i++){
-    cout << inventory[i] << endl;
-  }
-}
-
+//pick up item if room is not empty
 void pickUp() {
 
   int currentItem = strlen((storage[currentRoom])->getItem()); 
   int itemOrder;
+  char empty[15] = {'x',' ',' ',' ',' ',' ',' '};
 
   switch(currentItem) {
     case 2:
@@ -165,13 +173,88 @@ void pickUp() {
   }
 
   for (int i = 0; i < 5; i++){
-    if (strcmp(inventory[i], "x") == false) {
+    //if a inventory slot is empty, PROCEED
+    if (strcmp(inventory[i], "x") == false)
+    {
+    if (!((storage[currentRoom])->getItem()[0] == 'x')) {
       for (int a = 0; a < currentItem; a++){
         inventory[i][a]=items[itemOrder][a];
       }
       cout << endl << (storage[currentRoom])->getItem()  << " picked up!" << endl;
+      (storage[currentRoom])->setItem(empty);
       break;
+    }
     }
   }
 
+}
+
+//drop item from inventory if room is empty
+void drop() {
+
+}
+
+//see what items you have with you
+void printInventory() {
+  
+  cout << endl << "Inventory:" << endl;
+
+  for (int i = 0; i < 5; i++){
+    cout << inventory[i] << endl;
+  }
+}
+
+void printMap(int i) {
+  cout
+  << "Left: " << (storage[one])->getName()
+  << " Right: " << (storage[two])->getName()
+  << endl << "Top: " << (storage[three])->getName()
+  << " Bottom: " << (storage[four])->getName();
+  switch(i) {
+    case 0:
+      one = 2, two = 1, three = 3, four = 6;
+      break;
+    case 1:
+      one = 0, two = 13, three = 3, four = 7;
+      break;
+    case 2:
+      one = 0, two = 0, three = 0, four = 0;
+      break;
+    case 3:
+      one = 0, two = 0, three = 0, four = 1;
+      break;
+    case 4:
+      one = 6, two = 7, three = 8, four = 9;
+      break;
+    case 5:
+      one = 9, two = 10, three = 11, four = 12;
+      break;
+    case 6:
+      one = 1, two = 1, three = 1, four = 1;
+      break;
+    case 7:
+      one = 6, two = 6, three = 6, four = 6;
+      break;
+    case 8:
+      one = 9, two = 10, three = 11, four = 12;
+      break;
+    case 9:
+      one = 10, two = 11, three = 12, four = 13;
+      break;
+    case 10:
+      one = 11, two = 12, three = 13, four = 14;
+      break;
+    case 11:
+      one = 1, two = 1, three = 12, four = 13;
+      break;
+    case 12:
+      one = 1, two = 1, three = 11, four = 14;
+      break;
+    case 13:
+      one = 1, two = 1, three = 11, four = 14;
+      break;
+    case 14:
+      one = 13, two = 12, three = 11, four = 1;
+      break;
+  }
 }
